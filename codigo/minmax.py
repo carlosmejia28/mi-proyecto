402001afd782d793120ab08e7d9042c86d39d82e
Carlos <calbertomejia@unicesar.edu.co>
2021-09-11 17:29:47 -0500

    cambio nombre funciones min max

--- codigo/minmax.py
+++ codigo/minmax.py
@@ -1,11 +1,11 @@
-def min(valor1, valor2):
+def minimo(valor1, valor2):
     if valor1<valor2:
         return valor1
     else:
         return valor2 
 
 
-def max(valor1,valor2):
+def maximo(valor1,valor2):
     if valor1>valor2:
         return valor1
     else:
